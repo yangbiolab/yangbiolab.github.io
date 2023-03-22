9e70c2e651cfbadb7fcaa436c83e143632eec18d
Yang Lab <126637344+yangbiolab@users.noreply.github.com>
2023-03-23 01:05:02 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,35 +1,12 @@
 export default {
-  title: 'YangLab',
-  description: 'Yang Lab in FJMU',
-  base: "/",
-  // 主题配置
+  lang: 'en-US',
+  title: 'VitePress',
+  description: 'Vite & Vue powered static site generator.',
+
+  // Theme related configurations.
   themeConfig: {
-    siteTitle: 'YangLab@FJMU',
-    logo: '/background.jpeg',
-    socialLinks: [ {icon: 'github', link: 'https://github.com/yangbiolab' }],
-    // 文档所在的文件夹
-    docsDir: 'docs',
-    // 头部nav 
-    nav: [
-      {text: 'Aboult ', link: "/Aboult/"},
-      {text: 'Publications', link: "/Publications/"},
-      {text: 'Team', link: "/Team/"},
-      {text: 'Lab Fun', link: "/LabFun/"},
-      {text: 'News', link: "/News/"},
-      {text: 'Contact', link: "/Contact/"}
-    ],
-    sidebar: [
-      {
-        text: 'Guide',
-        items: [
-          { text: 'Introduction', link: '/introduction' },
-          { text: 'Getting Started', link: '/getting-started' }
-        ]
-      }
-    ]
-//     sidebar: {
-//       // 斜杠代表的是docs文件夹
-//       '/Aboult/': [ {text: "Goal", children: [{text: "gut microbiome", link: "/Aboult/goal"}] }]
-//     }
+    logo: '/logo.svg',
+    nav: [...],
+    sidebar: { ... }
   }
 }
