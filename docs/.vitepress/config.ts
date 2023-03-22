f138a7a68392b9a1479b82a310f71628cd3e67ac
Yang Lab <126637344+yangbiolab@users.noreply.github.com>
2023-03-23 01:02:02 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -23,8 +23,7 @@ export default {
         text: 'Guide',
         items: [
           { text: 'Introduction', link: '/introduction' },
-          { text: 'Getting Started', link: '/getting-started' },
-          ...
+          { text: 'Getting Started', link: '/getting-started' }
         ]
       }
     ]
